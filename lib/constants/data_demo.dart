e41b933a3e00a659714e8b31b60ea7a0cd507a7c
tub-git <atugitlab@gmail.com>
2026-09-12 08:48:55 +0700

    update flutter version

--- lib/constants/data_demo.dart
+++ lib/constants/data_demo.dart
@@ -1,7 +1,11 @@
-
-// {
-//   "id": 1,
-//   "fullName": "John",
-//   "email":"",
-//   "password":""
-// }
+List<Map<String, dynamic>> list = [
+  {
+    "id": 1,
+    "name": "Cherry Vanila",
+    "desc": "",
+    "score": 4.5,
+    "liked": true,
+    "image":
+        "https://cdn.prod.website-files.com/5ee0a01b09389eebf4c09b45/667c516ff83a3c815973676e_1-Before-Mobile.webp",
+  },
+];
