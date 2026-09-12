05671b2dcc8ed15e77632828854d8b8d3f8a7877
tub-git <atugitlab@gmail.com>
2026-09-12 14:46:40 +0700

    update dashboard and home page

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:learn_app/constants/app_color.dart';
+import 'package:learn_app/pages/category/category_page.dart';
+import 'package:learn_app/pages/home/home_page.dart';
+import 'package:learn_app/pages/order/order_page.dart';
+import 'package:learn_app/pages/profile/profile_page.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -8,23 +13,53 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  int selectIndex = 0;
+  List<Widget> _widget = [];
+
   @override
   void initState() {
-    // TODO: implement initState
+    _widget = [
+      HomePage(),
+      CategoryPage(),
+      OrderPage(),
+      ProfilePage(),
+    ];
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Dashbaord'),
+      body: _widget[selectIndex],
+      // body:  selectIndex == 0 ? HomePage(): selectIndex,
       bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedItemColor: AppColors.primaryColor,
+        unselectedItemColor: AppColors.textColor,
+        unselectedLabelStyle: TextStyle(color: AppColors.textColor),
+        selectedLabelStyle: TextStyle(color: AppColors.primaryColor),
+        onTap: (index) {
+          // index == 1
+          setState(() {
+            selectIndex = index;
+          });
+        },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled)),
-          BottomNavigationBarItem(icon: Icon(Icons.category_outlined)),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: "ໜ້າຫຼັກ",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category_outlined),
+            label: "ປະເພດ",
+          ),
 
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag)),
-          BottomNavigationBarItem(icon: Icon(Icons.person)),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: "ອໍເດີ",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "ໂປຣໄພ"),
         ],
       ),
     );
--- lib/pages/home/provider/home_logic.dart
+++ lib/pages/home/provider/home_logic.dart
@@ -1,6 +1,9 @@
-
 import 'package:flutter/material.dart';
+import 'package:learn_app/constants/data_demo.dart';
 
-class HomeLogic extends ChangeNotifier{
-  
+class HomeLogic extends ChangeNotifier {
+  Future<void> getListProduct() async {
+    List<Map<String, dynamic>> list = products;
+    try {} catch (e) {}
+  }
 }
